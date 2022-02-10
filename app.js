14c99d9e0d32285bc4379289021cd27753de115a
xavier-meyer <xavier.01@orange.fr>
2022-02-10 09:10:20 +0100

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+// fonction calculs()
+function addition(nb1, nb2){
+    let calcul = nb1+nb2;
+    alert(calcul);
+}
+function soustraction(nb1, nb2){
+    let calcul = nb1-nb2;
+    alert(calcul);
+}
+function division(nb1, nb2){
+    let calcul = nb1/nb2;
+    alert(calcul);
+}
+function multiplication(nb1, nb2){
+    let calcul = nb1*nb2;
+    alert(calcul);
+}
