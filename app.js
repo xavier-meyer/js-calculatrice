2262de2901b1a93b7c351b7c1816009797b60fcd
xavier-meyer <xavier.01@orange.fr>
2022-02-10 09:51:09 +0100

    feat: fonctionsNombre

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // fonction calculs()
 function addition(nb1, nb2){
     let calcul = nb1+nb2;
@@ -14,4 +15,21 @@ function division(nb1, nb2){
 function multiplication(nb1, nb2){
     let calcul = nb1*nb2;
     alert(calcul);
+}
+//function checkNombre()
+function checkNombre(nb1, nb2){
+    if(isNaN(nb1) || isNaN(nb2)){
+        alert("Veuillez saisir des nombres");
+        calculatrice();
+    }
+}
+//function demandeNombre()
+function demandeNombre(){
+    nb1 = prompt("Veuillez saisir le premier nombre de l'opération");
+    nb2 = prompt("Veuillez saisir le deuxième nombre de l'opération");
+}
+//function conversionNombre()
+function conversionNombre(nb1,nb2){
+    nbConvert1 = parseInt(nb1);
+    nbConvert2 = parseInt(nb2);
 }
