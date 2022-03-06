1ec65792b3629921c0ce5b57eca0badebb8512f2
xavier-meyer <xavier.01@orange.fr>
2022-03-06 07:26:33 +0100

    feat: modif noms de fonctions ds la fct calculatrice

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ calculatrice();
 function calculatrice(){
     let choix = prompt("Bienvenue dans la super calculatrice!\nVeuillez choisir une option:\n1 réaliser une addition\n2 réaliser une soustraction\n3 réaliser une division\n4 réaliser une multiplication");  
     if(choix == 1){
-        demander(); 
+        demandeNombre(); 
         checkNombre(nb1,nb2); 
-        convertirNombre(nb1,nb2);
+        conversionNombre(nb1,nb2);
         addition(nbtransform1, nbtransform2);        
     }else if (choix == 2){
-        demander();   
+        demandeNombre();   
         checkNombre(nb1,nb2);     
-        convertirNombre(nb1,nb2);
+        conversionNombre(nb1,nb2);
         soustraction(nbtransform1, nbtransform2);                 
     }else if (choix == 3){ 
-        demander(); 
+        demandeNombre(); 
         checkNombre(nb1,nb2);       
-        convertirNombre(nb1,nb2);
+        conversionNombre(nb1,nb2);
         division(nbtransform1, nbtransform2); 
     }else if (choix == 4){
-        demander();
+        demandeNombre();
         checkNombre(nb1,nb2);    
-        convertirNombre(nb1,nb2);
+        conversionNombre(nb1,nb2);
         multiplication(nbtransform1, nbtransform2);
     }else{
         if(confirm('Veuillez saisir une opération valide. Appuyez sur "OK" pour recommencer ou "Annuler" pour quitter')){
@@ -64,6 +64,6 @@ function demandeNombre(){
 }
 //function conversionNombre()
 function conversionNombre(nb1,nb2){
-    nbConvert1 = parseInt(nb1);
-    nbConvert2 = parseInt(nb2);
+    nbtransform1  = parseInt(nb1);
+    nbtransform2 = parseInt(nb2);
 }
